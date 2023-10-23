20453d6541115f38672a4037c04f0b3db6850141
yikZero <i@yikzero.com>
2023-10-23 13:50:20 +0800

    feat(SideBar): 新增图表入场动画

--- src/pages/EChartsSideBar.tsx
+++ src/pages/EChartsSideBar.tsx
@@ -2,19 +2,54 @@ import Title from "../components/atom/Title";
 import SpecializedBusiness from "../components/SpecializedBusiness";
 import BasicBar from "../components/BasicBar";
 import NightingaleChart from "../components/NightingaleChart";
+import { motion } from "framer-motion";
 
 function EChartsSideBar() {
+  const listMotion = {
+    visible: {
+      opacity: 1,
+      transition: {
+        type: "spring",
+        staggerChildren: 0.1,
+      },
+    },
+    hidden: {
+      opacity: 0,
+    },
+  };
+
+  const itemMotion = {
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: -100 },
+  };
 
   return (
     <>
-      <section className="absolute right-6 top-6 w-[480px] p-6 flex flex-col gap-3">
-        <Title title="专项业务"/>
-        <SpecializedBusiness />
-        <Title title="产品订购"/>
-        <BasicBar />
-        <Title title="电子合同"/>
-        <NightingaleChart />
-      </section>
+      <motion.section
+        initial="hidden"
+        animate="visible"
+        variants={listMotion}
+        className="absolute right-6 top-6 w-[480px] p-6 flex flex-col gap-3"
+      >
+        <motion.div variants={itemMotion}>
+          <Title title="专项业务" />
+        </motion.div>
+        <motion.div variants={itemMotion}>
+          <SpecializedBusiness />
+        </motion.div>
+        <motion.div variants={itemMotion}>
+          <Title title="产品订购" />
+        </motion.div>
+        <motion.div variants={itemMotion}>
+          <BasicBar />
+        </motion.div>
+        <motion.div variants={itemMotion}>
+          <Title title="电子合同" />
+        </motion.div>
+        <motion.div variants={itemMotion}>
+          <NightingaleChart />
+        </motion.div>
+      </motion.section>
     </>
   );
 }
